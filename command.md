e144e3782b68a94684722bba7fcad073e747caa4
Haikal Fikri <haikaljr1245@gmail.com>
2025-02-27 22:58:35 +0700

    add command to command.md file, setup SECRET_KEY in settings to .env

--- command.md
+++ command.md
@@ -1,11 +1,13 @@
-## Create Project
+# Commands
+
+pip install requirement.txt
+
 django-admin startproject drfecommerce
 
-## Run django app
 python manage.py runserver
 
-## Import get_random_secret_key
 from django.core.management.utils import get_random_secret_key
 
-# Print value from get_random_secret_key
 print(get_random_secret_key())
+
+pip install --upgrade pip
--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "@+73q&2r*_hgit*peom(et#1o^acbj&ld10-5oi7@0cd0-)^_e"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
